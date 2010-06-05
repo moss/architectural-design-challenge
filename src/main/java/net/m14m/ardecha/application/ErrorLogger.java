548bb96b239b5d6a6fb07412284b4df67d906d0d
Moss Collum <moss.collum@gmail.com>
2010-06-05 13:19:09 -0400

    Not the greatest file not found message in the world, but it gets the point across pretty cleanly.

--- src/main/java/net/m14m/ardecha/application/ErrorLogger.java
+++ src/main/java/net/m14m/ardecha/application/ErrorLogger.java
@@ -1,6 +1,6 @@
 package net.m14m.ardecha.application;
 
-import java.io.PrintWriter;
+import java.io.*;
 
 public class ErrorLogger {
     private final PrintWriter ouputWriter;
@@ -10,8 +10,21 @@ public class ErrorLogger {
     }
 
     public void log(Exception e) {
+        // if this gets bigger consider making some sort of collection of ErrorFormatters
+        if (e instanceof FileNotFoundException) {
+            printFileNotFoundMessage(e);
+        } else {
+            printGenericMessage(e);
+        }
+        ouputWriter.flush();
+    }
+
+    private void printGenericMessage(Exception e) {
         ouputWriter.println("An error occurred.");
         e.printStackTrace(ouputWriter);
-        ouputWriter.flush();
+    }
+
+    private void printFileNotFoundMessage(Exception e) {
+        ouputWriter.println("File not found: " + e.getMessage());
     }
 }
--- src/test/java/net/m14m/ardecha/application/ErrorLoggerTest.java
+++ src/test/java/net/m14m/ardecha/application/ErrorLoggerTest.java
@@ -1,5 +1,6 @@
 package net.m14m.ardecha.application;
 
+import org.hamcrest.Matcher;
 import org.junit.*;
 
 import java.io.*;
@@ -11,6 +12,7 @@ import static org.mockito.Mockito.*;
 
 @SuppressWarnings({"ThrowableInstanceNeverThrown"})
 public class ErrorLoggerTest {
+    private static final String GENERIC_ERROR_MESSAGE = "An error occurred.";
     private StringWriter outputWriter = spy(new StringWriter());
     private ErrorLogger errorLogger;
 
@@ -20,14 +22,37 @@ public class ErrorLoggerTest {
 
     @Test public void shouldPrintAMessageAboutTheError() {
         errorLogger.log(new RuntimeException("I haz a ouch."));
-        String output = outputWriter.toString();
-        assertThat("Should start with readable message", output, startsWith("An error occurred."));
-        assertThat("Should include the exception", output, containsString("RuntimeException"));
-        assertThat("Should include the exception message", output, containsString("I haz a ouch"));
+        outputShouldStartWith("readable message", GENERIC_ERROR_MESSAGE);
+        outputShouldContain("the exception", "RuntimeException");
+        outputShouldContain("the exception message", "I haz a ouch");
     }
 
     @Test public void shouldFlushAfterOutput() {
         errorLogger.log(new RuntimeException());
         verify(outputWriter).flush();
+    }
+
+    @Test public void shouldFormatFileNotFoundExceptionsWithAFriendlyErrorMessage() {
+        errorLogger.log(new FileNotFoundException("dir/badfile (No such file or directory)"));
+        outputShouldNotContain("the generic message", GENERIC_ERROR_MESSAGE);
+        outputShouldNotContain("a stack trace", "at java.");
+        outputShouldContain("a meaningful message",
+                "File not found: dir/badfile (No such file or directory)");
+    }
+
+    private void outputShouldStartWith(String roleOfText, String expectedText) {
+        ensureThatOutput("start with " + roleOfText, startsWith(expectedText));
+    }
+
+    private void outputShouldContain(String roleOfText, String expectedText) {
+        ensureThatOutput("include " + roleOfText, containsString(expectedText));
+    }
+
+    private void outputShouldNotContain(String roleOfText, String expectedAbsentText) {
+        ensureThatOutput("not include " + roleOfText, not(containsString(expectedAbsentText)));
+    }
+
+    private void ensureThatOutput(String requirement, Matcher<String> matcher) {
+        assertThat("Output should " + requirement, outputWriter.toString(), matcher);
     }
 }
