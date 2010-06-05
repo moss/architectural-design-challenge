802cbe88f8a79eb5fc3817187ad6866b5d2eaeca
Moss Collum <moss.collum@gmail.com>
2010-06-05 15:07:21 -0400

    Removed a stray TODO.

--- src/main/java/net/m14m/ardecha/output/Output.java
+++ src/main/java/net/m14m/ardecha/output/Output.java
@@ -3,6 +3,5 @@ package net.m14m.ardecha.output;
 import net.m14m.ardecha.characters.TranslatableCharacter;
 
 public interface Output {
-    // TODO primitive argument
     void writeChar(TranslatableCharacter translatableCharacter);
 }
