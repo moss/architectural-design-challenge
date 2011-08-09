7732cb7cb3342e86b8d97e5740f0b408b92cf5ad
Moss Collum <moss.collum@gmail.com>
2011-08-09 09:20:34 -0400

    Started wiring up the application. End to end test now getting significantly further--in retrospect, I wish I'd started this earlier.

--- src/main/java/net/m14m/ardecha/Rot13Application.java
+++ src/main/java/net/m14m/ardecha/Rot13Application.java
@@ -1,10 +1,16 @@
 package net.m14m.ardecha;
 
+import java.io.File;
+
 public class Rot13Application {
     private final FileRepository fileRepository;
     private final OutputRepository outputRepository;
 
     public static void main(String[] args) {
+        File ioDirectory = new File(System.getProperty("io-directory"));
+        DirectoryBackedFileRepository fileRepository = new DirectoryBackedFileRepository(ioDirectory);
+        Rot13Application application = new Rot13Application(fileRepository, null);
+        application.run(args[0], args[1]);
     }
 
     public Rot13Application(FileRepository fileRepository, OutputRepository outputRepository) {
